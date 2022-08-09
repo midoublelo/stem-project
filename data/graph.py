c397047959c11aafa508581b4083c74728cd9b57
Millo Evers <exedysdigital@gmail.com>
2022-08-09 10:25:44 +0100

    Cross-platform CSV loading and option to change map colour
    CSV loading format now automatically changes depending on OS used as Windows and Unix file paths are in a different format and colour of the map for the 'globalVaccines' dataset can now be changed.

--- data/graph.py
+++ data/graph.py
@@ -1,36 +1,65 @@
 import pandas as pd
 import plotly.express as px
+from sys import platform
 
-COLOURSCHEME = px.colors.sequential.algae
+# COLOURSCHEME = px.colors.sequential.algae
 
-def generateGraph(dataset: str, mode: str="html"):
+def loadCSV(dataset: str):
+	if platform == "linux" or platform == "linux2":
+		strFormat = rf"./data/csv/{dataset}.csv"
+	elif platform == "win32":
+		strFormat = rf".\csv\{dataset}.csv"
+	csvLoaded = pd.read_csv(strFormat)
+	return csvLoaded
+
+def generateGraph(dataset: str, mode: str = "html", colour: str = "algae"):
     '''
     Generates a graph for given dataset
+
+ 	Parameters:
+ 		dataset: Choose the dataset to be plotted
+  		mode: Choose the export mode you prefer
+   		colour: Choose the colour scheme of the map (applies to globalVaccines only)
     '''
     print(f"Generating graph for '{dataset}' in mode '{mode}'")
     if dataset == "londonCases":
-        df = pd.read_csv(r".\csv\londonCases.csv") # Loads data from csv and makes a dataframe in pandas
-        fig = px.line(df, x = 'date', y = 'cumCasesBySpecimenDate', title='Daily New Cases in London', labels={
+        csvLoaded = loadCSV(dataset)
+        df = csvLoaded  # Loads data from csv and makes a dataframe in pandas
+        fig = px.line(
+            df,
+            x='date',
+            y='cumCasesBySpecimenDate',
+            title='Daily New Cases in London',
+            labels={
                 "date": "Date",
                 "cumCasesBySpecimenDate": "Total Cases"
             }
-        ) # Creates a line graph using the 'date' and 'cumCasesBySpecimenDate' columns from the csv
+        )  # Creates a line graph using the 'date' and 'cumCasesBySpecimenDate' columns from the csv
     elif dataset == "usaCases":
-        df = pd.read_csv(r".\csv\usaCases.csv")
-        fig = px.line(df, x = 'date', y = 'total_cases', title='Daily New Cases in USA', labels={
-                "date": "Date",
-                "total_cases": "Total Cases"
-            }
-        )
+        csvLoaded = loadCSV(dataset)
+        df = csvLoaded
+        fig = px.line(df,
+                      x='date',
+                      y='total_cases',
+                      title='Daily New Cases in USA',
+                      labels={
+                          "date": "Date",
+                          "total_cases": "Total Cases"
+                      })
     elif dataset == "englandDeaths":
-        df = pd.read_csv(r".\csv\englandDeaths.csv")
-        fig = px.line(df, x = 'week', y = 'deaths', title='Weekly Covid Deaths in England', labels={
-                "week": "Week Ending",
-                "deaths": "Weekly Deaths"
-            }
-        )
+        csvLoaded = loadCSV(dataset)
+        df = csvLoaded
+        fig = px.line(df,
+                      x='week',
+                      y='deaths',
+                      title='Weekly Covid Deaths in England',
+                      labels={
+                          "week": "Week Ending",
+                          "deaths": "Weekly Deaths"
+                      })
     elif dataset == "globalVaccines":
-        df = pd.read_csv(r".\csv\globalVaccines.csv")
+        csvLoaded = loadCSV(dataset)
+        df = csvLoaded
         fig = px.choropleth(
             df,
             locations="ISO3",
@@ -39,7 +68,7 @@ def generateGraph(dataset: str, mode: str="html"):
             # hover_data={'VACCINES_USED', 'FIRST_VACCINE_DATE'},
 			hover_data={'FIRST_VACCINE_DATE'},
             title="Global Vaccinations",
-            color_continuous_scale=COLOURSCHEME,
+            color_continuous_scale=colour, #COLOURSCHEME,
             labels={
                 "PERSONS_FULLY_VACCINATED_PER100":
                 "% of Population Vaccinated",
@@ -47,18 +76,23 @@ def generateGraph(dataset: str, mode: str="html"):
                 "FIRST_VACCINE_DATE": "Date of First Vaccination",
                 #"VACCINES_USED": "List of Vaccine Types used",
             })
-        # Creates a choropleth map of the vaccination data across the world based on the percentage of people fully vaccinated. 
+        # Creates a choropleth map of the vaccination data across the world based on the percentage of people fully vaccinated.
         # Also shows date of first vaccination and list of vaccine types used
         fig["layout"].pop("updatemenus")
     if mode == "web":
-        fig.show(renderer="browser") # Opens in the browser
+        fig.show(renderer="browser")  # Opens in the browser
     elif mode == "html":
-        fig.write_html(f"{dataset}.html", auto_open=True)  # Writes to static html file - Default
+        fig.write_html(f"{dataset}.html",
+                       auto_open=True)  # Writes to static html file - Default
     elif mode == "html-connected":
-        fig.write_html(f"{dataset}.html", include_plotlyjs="cdn", auto_open=True)  # Writes to html file with a much smaller file size, requires internet connection
+        fig.write_html(
+            f"{dataset}.html", include_plotlyjs="cdn", auto_open=True
+        )  # Writes to html file with a much smaller file size, requires internet connection - Recommended
     elif mode == "png":
-        fig.show(renderer="png") # Creates png image - Requires kaleido library and ipython
+        fig.show(renderer="png"
+                 )  # Creates png image - Requires kaleido library and ipython
     elif mode == "auto":
-        fig.show() # Let plotly automatically decide what renderer to use
-    
-generateGraph("globalVaccines")
+        fig.show()  # Let plotly automatically decide what renderer to use
+
+
+generateGraph("globalVaccines", "html", "algae")
